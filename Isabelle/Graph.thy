77c3852ce96ce8c05f9dd36c9474acc216a33401
Vincent Trélat <vincent.trelat@free.fr>
2021-11-18 17:48:32 +0100

    some syntax issues

--- Isabelle/Graph.thy
+++ Isabelle/Graph.thy
@@ -44,28 +44,26 @@ lemma reachable_trans:
 function unite :: "'v \<Rightarrow> 'v \<Rightarrow> 'v env" where
   "unite v w e =
       (if (S e v = S e w) then e
-      else let r = hd(stack e) and r' = hd(tl(stack e)) and  e' = e \<lparr> stack := tl(stack e) and s:=s e (r <- s e r \<union> s e r', r' <- s e r \<union> s e r') \<rparr>
+      else let r = hd(stack e);
+               r'= hd(tl(stack e));
+               e'= e \<lparr> stack := tl(stack e), S := S e (r \<leftarrow> S e r \<union> S e r', r' \<leftarrow> S e r \<union> S e r')\<rparr>
           in unite v w e')"
 
 function dfs :: "'v \<Rightarrow> 'v env \<Rightarrow> 'v env" and dfss :: "'v \<Rightarrow> 'v set \<Rightarrow> 'v env \<Rightarrow> 'v env" where
   "dfs v e =
-  (let e1 = e1\<lparr>visited := visited e \<union> {v},
-              stack := (v :: stack e)\<rparr> in let
-      e' = dfss (succ v) e1
+  (let e1 = e1\<lparr>visited := visited e \<union> {v}, stack := (v # stack e)\<rparr>;
+       e' = dfss (succ v) e1
   in if v = hd(stack e')
-      then e'\<lparr>sccs:=sccs e' \<union> (S e v),
-              explored:=explored e' \<union> (S e u),
-              stack:=tl(stack e')\<rparr>
+      then e'\<lparr>sccs:=sccs e' \<union> (S e v), explored:=explored e' \<union> (S e u), stack:=tl(stack e')\<rparr>
     else e')"
 | "dfss v vs e =
-   (if vs={} then e
-   else let w = SOME x. x \<in> vs
-     in if w \<in> explored e
-        then dfss v (vs\<setminus>{w}) e
+   (if vs = {} then e
+   else (let w = SOME x. x \<in> vs
+     in (if w \<in> explored e
+        then dfss v (vs-{w}) e
       else if w \<notin> visited e
         then dfs w e
-      else unite v w e)"
-  by pat_comleteness auto
+      else unite v w e)))"
 
 end
 end
