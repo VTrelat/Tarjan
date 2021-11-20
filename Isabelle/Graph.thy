03c137955c91d3811789e7dbe7ba3b63d5c7757e
Vincent Trélat <vincent.trelat@free.fr>
2021-11-20 12:16:08 +0100

    dfs preconditions and well-formed env updates

--- Isabelle/Graph.thy
+++ Isabelle/Graph.thy
@@ -50,6 +50,11 @@ function unite :: "'v \<Rightarrow> 'v \<Rightarrow> 'v env \<Rightarrow> 'v env
                e'= e \<lparr> stack := tl(stack e), S := (S e) (r := joined, r' := joined)\<rparr>
           in unite v w e')"
   by pat_completeness auto
+(*
+pat_completeness : ensures that the function is complete, i.e. every entry in the domain is covered.
+auto : proves that unite is actually a function, so that for two equal entries, the computed results for both entries are equal.
+The termination is not proved.
+*)
 
 function dfs :: "'v \<Rightarrow> 'v env \<Rightarrow> 'v env" and dfss :: "'v \<Rightarrow> 'v set \<Rightarrow> 'v env \<Rightarrow> 'v env" where
   "dfs v e =
@@ -68,16 +73,34 @@ function dfs :: "'v \<Rightarrow> 'v env \<Rightarrow> 'v env" and dfss :: "'v \
       else unite v w e)))"
   by pat_completeness (force+)
 
+(*
+force : proves that dfs and dfss are functions. Method auto cannot terminate because of the mutual recursion.
+The termination is not proved.
+*)
+
 text \<open>
   Well-formed environments.
 \<close>
-definition wf_env where "wf_env e \<equiv> True"
+definition wf_env where
+  "wf_env e \<equiv>
+    distinct (stack e)
+  \<and> set (stack e) \<supseteq> visited e
+  "
+(*
+Maybe add precedes (\<preceq>) def, cf L.543 - L.683 in Tarjan.thy
+*)
+(*
+Definitions will help later in the proof in pre/post-conditions to ensure that all parameters are well-formed and fit the definitions.
+It seems natural but Isabelle needs accurate details.
+*)
 
 text \<open>
   Precondition for function dfs.
 \<close>
-definition pre_dfs where "pre_dfs v e \<equiv> wf_env e"
-
+definition pre_dfs where "pre_dfs v e \<equiv> wf_env e \<and> v \<notin> visited e"
+(*
+Preconditions will appear in the proof like the following: a lemma assumes a predcond and shows a postcond.
+*)
 text \<open>
   Precondition for function dfss.
 \<close>
@@ -86,7 +109,7 @@ definition pre_dfss where "pre_dfss v vs e \<equiv> wf_env e"
 lemma pre_dfs_pre_dfss:
   assumes "pre_dfs v e"
   shows "pre_dfss v (successors v) (e \<lparr> visited := visited e \<union> {v}, stack := v # stack e\<rparr>)"
-  sorry
+  using assms unfolding pre_dfs_def pre_dfss_def wf_env_def by auto
 
 end
 end
